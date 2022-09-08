a2125c3edeb39dcd3fbb6f8e65c9843d1da130b4
Darius Weiberg <dariusweiberg@gmail.com>
2022-09-08 17:52:03 +0200

    Leveraged Redux Persist to persist cart items

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-export const store = createStore(rootReducer, undefined, applyMiddleware(logger));
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
