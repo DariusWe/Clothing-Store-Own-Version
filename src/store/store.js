8dc3c8f73631ba23ec446f658ea4ed28dbfaf936
Darius Weiberg <dariusweiberg@gmail.com>
2022-09-27 20:34:25 +0200

    leveraged redux-persist-transform-filter to only persist cartItems (not isCartOpen) and added logger to defaultMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
+import { createFilter } from "redux-persist-transform-filter";
+import logger from "redux-logger";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -8,10 +10,13 @@ Most of the code here is stupid boilerplate code to make persisting state with r
 https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
 */
 
+const cartSubsetFilter = createFilter("cart", ["cartItems"]);
+
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
+  transforms: [cartSubsetFilter],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -23,7 +28,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }),
+    }).concat(process.env.NODE_ENV === "development" && logger),
 });
 
 export const persistor = persistStore(store);
